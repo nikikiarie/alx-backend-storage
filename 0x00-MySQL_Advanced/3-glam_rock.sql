86e5a584949c111536c4364e838d7909530611f8
nikikiarie <nix.kiariebiz@gmail.com>
2024-11-08 00:08:13 +0300

    comment

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
--- List Glam bands by how long they've been together, ranked by years active
+-- List Glam bands by how long, ranked by years active
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0
